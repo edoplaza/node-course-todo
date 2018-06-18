11aa5c4d68cf39bf9c6d707bfaf3da1b0e3dd758
Eduardo Plaza <edoplaza@gmail.com>
2018-06-18 18:31:18 +0200

    Add tests for GET /todos

--- server/server.js
+++ server/server.js
@@ -12,13 +12,13 @@ app.use(bodyParser.json());
 app.post('/todos', (req, res) => {
   var todo = new Todo({
     text: req.body.text
-  })
+  });
 
   todo.save().then((doc) => {
     res.send(doc);
   }, (e) => {
     res.status(400).send(e);
-  })
+  });
 });
 
 app.get('/todos', (req, res) => {
